8ab4ae3c8c0a8b1cae0c0f4ba0cbd18f9cbf4a1b
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-06-26 04:38:28 +0530

    Completed Dice App with SwiftUI

--- DiceeSwiftUI/ContentView.swift
+++ DiceeSwiftUI/ContentView.swift
@@ -9,8 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber:Int = 1
+    @State var rightDiceNumber:Int = 5
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack{
+                    DiceView(num: leftDiceNumber)
+                    DiceView(num:rightDiceNumber)
+                }.padding(.horizontal, 35.0)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }.background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    let num:Int
+    
+    var body: some View {
+        Image("dice\(num)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding()
     }
 }
 
